e7d575f8060e2d0d724b8215a88b8ff209a8032b
Shubham <shubhamsinha2050@gmail.com>
2019-10-25 01:33:00 +0530

    readonly

--- form.js
+++ form.js
@@ -3,3 +3,4 @@ import { formValid } from './formValidation.js'
 var a = new formValid();
 console.log(a);
 a.setMandatory("firstname", true);
+a.setReadOnly("firstname", true);
--- formValidation.js
+++ formValidation.js
@@ -12,6 +12,9 @@ formValid.prototype.setMandatory = function(value, status) {
             clearInterval(set);
             if (element.length == 1) {
                 if (status) {
+                    // if (element[0].hasAttribute("readonly")) { 
+                    // console.error("Can't be set to mandatory as the field is readonly")
+                    // }
                     element[0].setAttribute("required", true);
                 } else {
                     element[0].removeAttribute("required");
@@ -30,3 +33,30 @@ formValid.prototype.setMandatory = function(value, status) {
         }
     }, 1);
 }
+
+
+formValid.prototype.setReadOnly = function(value, status) {
+    var element = document.getElementsByName("firstname");
+    var set = setInterval(function readonly() {
+        if (document.readyState == "complete") {
+            clearInterval(set);
+            if (element.length == 1) {
+                if (status === true) {
+                    element[0].style.backgroundColor = "#ddd";
+                    element[0].setAttribute("readonly", true);
+                } else if (status === false) {
+                    if (element[0].hasAttribute("readonly")) {
+                        element[0].removeAttribute("readonly");
+                    }
+                } else {
+                    console.log("Should be boolean");
+                }
+            } else {
+                console.log("error for length");
+            }
+        }
+    }, 1)
+}
+
+// [0].style.backgroundColor = "black"
+// document.getElementsByName("firstname")[0].setAttribute("readonly", true)
